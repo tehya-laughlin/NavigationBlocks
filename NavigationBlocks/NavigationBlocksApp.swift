a58987f5f356532b2348f573f93c26cc9acb9e1e
tehya-laughlin <tehyanlaugh.shop@gmail.com>
2023-09-12 16:43:22 -0600

    Initial Commit

---
+++ NavigationBlocks/NavigationBlocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationBlocksApp.swift
+//  NavigationBlocks
+//
+//  Created by Tehya Laughlin on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationBlocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
